531dd2dcc5fb8ff42b3be2cd740fe470b74234c4
Ajaz Ul Haq <ajaz.ul.haq@ladybirdweb.com>
2022-03-29 11:47:52 +0530

    Added Icon, and Code Optimization

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="icon" href="{{ url('favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+
 </head>
 <body style="padding-top: 40px">
 
@@ -106,7 +110,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="success"></div>
+            <div class="success alert-success">
+            </div>
             <div class="card">
                 <div class="card-header">
                     <h4>Students List
@@ -196,6 +201,7 @@
                     if(response.status == 400){
                         $('.errors').addClass('alert alert-danger');
                         $('.errors').append('Form Validation failed<br>');
+
                     }
                     else{
 
@@ -211,8 +217,6 @@
                         $('#addStudentModal').modal('hide');
                         getStudents();
 
-
-                        $( ".success" ).delay(2000).fadeOut();
                     }
                 }
             });
@@ -263,13 +267,11 @@
                     if (response.status == 400) {
                         $('.errors').addClass('alert alert-danger');
                         $('.errors').append('Form Validation failed<br>');
-                        $('.errors').delay(2000).fadeOut();
                     } else {
                         $('.success').addClass('alert alert-success');
                         $('.success').append('User Added Successfully!!');
                         $('#editStudentModal').modal('hide');
                         getStudents();
-                        $(".success").delay(2000).fadeOut();
                     }
                 }
             });
@@ -298,7 +300,6 @@
                         $('.success').append('User Deleted Successfully!!');
                         $('#deleteStudentModal').modal('hide');
                         getStudents();
-                        $( ".success" ).delay(3000).fadeOut();
                     }
                     else{
                         $('.errors').addClass('alert alert-danger');
