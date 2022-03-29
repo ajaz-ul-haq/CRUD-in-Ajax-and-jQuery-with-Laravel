14c1eefc8ea567db6224d25be4c1691a4ac1cfbc
Ajaz Ul Haq <ajaz.ul.haq@ladybirdweb.com>
2022-03-29 11:12:39 +0530

    Factories, Seeder, Test Cases, and Code Optimization

---
+++ database/factories/studentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\student>
+ */
+class studentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => random_int(7000000000, 9999999999),
+        ];
+    }
+}
